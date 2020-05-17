7f92304200bac62062e9fc3b2c7f9117794e9a4d
Radziej113 <r.bialek88@gmail.com>
2020-05-17 11:36:18 +0200

    6.2_zadanie

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main (String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+
+        int a = 2;
+        int b = 1;
+
+        Calculator simpleCalculator = new Calculator();
+
+        Integer resultAdd = simpleCalculator.add(a,b);
+
+        if(resultAdd.equals(3)){
+
+            System.out.println("Addition est passed!");
+
+        } else {
+
+            System.out.println("ERROR!");
+        }
+
+        Integer resultSubtract = simpleCalculator.subtract(a,b);
+
+        if(resultSubtract.equals(1)){
+
+            System.out.println("Subtraction test passed!");
+
+        } else {
+
+            System.out.println("ERROR!");
+        }
     }
 }
